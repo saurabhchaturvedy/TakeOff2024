8633e7238d950ff566f55eaf4d2ec8cee52cae35
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-05-14 14:32:48 +0530

    takeoff 2024

--- src/main/java/BackToBasics/SinglyLinkedList/SinglyLinkedList.java
+++ src/main/java/BackToBasics/SinglyLinkedList/SinglyLinkedList.java
@@ -16,6 +16,31 @@ public class SinglyLinkedList {
     }
 
 
+    public int size(ListNode head) {
+
+        if (head == null) return 0;
+        ListNode temp = head;
+        int length = 0;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+
+
+    public ListNode insertAtStart(ListNode head, int data) {
+
+        if (head == null) return new ListNode(data);
+
+        ListNode newNode = new ListNode(data);
+        newNode.next = head;
+        head = newNode;
+        return head;
+
+    }
+
+
     public static void main(String[] args) {
 
 
@@ -32,5 +57,11 @@ public class SinglyLinkedList {
         third.next = fourth;
 
         singlyLinkedList.print(head);
+
+        System.out.println("Size of the linked list is : " + singlyLinkedList.size(head));
+
+
+        ListNode listNode = singlyLinkedList.insertAtStart(head, 35);
+        singlyLinkedList.print(listNode);
     }
 }
