d4c21721d0bcedacc1ed91c0405b25a0f6f96289
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-05-19 11:22:01 +0530

    takeoff 2024

---
+++ src/main/java/BackToBasics/DoublyLinkedList/ListNode.java
@@ -0,0 +1,16 @@
+package BackToBasics.DoublyLinkedList;
+
+public class ListNode {
+
+
+    int data;
+    ListNode previous;
+    ListNode next;
+
+
+    ListNode(int data) {
+        this.data = data;
+        this.previous = null;
+        this.next = null;
+    }
+}
