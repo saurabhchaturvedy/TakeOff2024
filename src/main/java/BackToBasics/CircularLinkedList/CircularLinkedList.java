82df87891b303392192abb9d680d9376371b550e
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-05-25 16:47:37 +0530

    takeoff 2024

---
+++ src/main/java/BackToBasics/CircularLinkedList/CircularLinkedList.java
@@ -0,0 +1,59 @@
+package BackToBasics.CircularLinkedList;
+
+import BackToBasics.SinglyLinkedList.ListNode;
+
+public class CircularLinkedList {
+
+
+    private ListNode last;
+    private int length;
+
+
+    private class ListNode {
+
+        int data;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    public CircularLinkedList() {
+        this.last = null;
+        this.length = 0;
+    }
+
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+
+    private void createCircularLinkedList() {
+
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+
+        last = fourth;
+    }
+
+
+    public static void main(String[] args) {
+
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+        circularLinkedList.createCircularLinkedList();
+    }
+}
