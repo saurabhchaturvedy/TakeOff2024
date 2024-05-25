58e39742fa9419adbc687571fa934db154b4057b
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-05-25 20:16:24 +0530

    takeoff 2024

---
+++ src/main/java/BackToBasics/Arrays/PalindromeCheck.java
@@ -0,0 +1,28 @@
+package BackToBasics.Arrays;
+
+public class PalindromeCheck {
+
+
+    private boolean isPalindrome(String str) {
+
+        char[] chars = str.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+
+            if (chars[start] != chars[end]) return false;
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
