05e3df69997f1226dc5891982c9ec56b7722484d
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-05-28 11:27:30 +0530

    takeoff 2024

---
+++ src/main/java/BackToBasics/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,19 @@
+package BackToBasics.BinarySearchTree;
+
+public class BinarySearchTree {
+
+
+    TreeNode root;
+
+    private static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+
+        TreeNode(int data) {
+
+            this.data = data;
+        }
+    }
+}
