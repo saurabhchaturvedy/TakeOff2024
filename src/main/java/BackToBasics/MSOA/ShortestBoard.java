4c5d5002dd4f0fd162a22ca91ffcba72eaa18695
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-07-05 18:06:20 +0530

    takeoff 2024

---
+++ src/main/java/BackToBasics/MSOA/ShortestBoard.java
@@ -0,0 +1,38 @@
+package BackToBasics.MSOA;
+
+import java.util.Arrays;
+
+public class ShortestBoard {
+
+
+    public int solution(int[] arr) {
+
+
+        Arrays.sort(arr);
+
+        int min = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) {
+            int leftSize = arr[i] - arr[0];
+            int rightSize = arr[arr.length - 1] - arr[i + 1];
+            int max = Math.max(leftSize, rightSize);
+            if (max < min) {
+                min = max;
+            } else {
+                break;
+            }
+        }
+
+
+        return min;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {11, 20, 15};
+
+        ShortestBoard shortestBoard = new ShortestBoard();
+
+        System.out.println(shortestBoard.solution(arr));
+    }
+}
