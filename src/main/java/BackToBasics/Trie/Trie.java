5179a6b17bc1c071742735ff09f9d7ae6e180976
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-07-07 15:14:22 +0530

    takeoff 2024

--- src/main/java/BackToBasics/Trie/Trie.java
+++ src/main/java/BackToBasics/Trie/Trie.java
@@ -1,5 +1,8 @@
 package BackToBasics.Trie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Trie {
 
 
@@ -53,6 +56,74 @@ public class Trie {
         current.isEndOfTheWord = true;
     }
 
+    public boolean search(String word) {
+        if (word == null || word.isEmpty()) {
+            throw new IllegalArgumentException("Word is empty !!");
+        }
+
+        TrieNode current = root;
+        word = word.toLowerCase();
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return current.isEndOfTheWord;
+    }
+
+
+    public List<String> autocomplete(String prefix) {
+        if (prefix == null || prefix.isEmpty()) return new ArrayList<>();
+
+
+        prefix = prefix.toLowerCase();
+
+        TrieNode current = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+
+            char c = prefix.charAt(i);
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                return new ArrayList<>();
+            }
+
+            current = current.children[index];
+        }
+
+        List<String> results = new ArrayList<>();
+        searchSimilarPrefixWords(current, new StringBuilder(prefix), results);
+
+        return results;
+    }
+
+    private void searchSimilarPrefixWords(TrieNode current, StringBuilder prefix, List<String> results) {
+
+        if (current.isEndOfTheWord) {
+            results.add(prefix.toString());
+        }
+
+
+        for (char c = 'a'; c <= 'z'; c++) {
+
+            int index = c - 'a';
+            if (current.children[index] != null) {
+
+                prefix.append(c);
+                searchSimilarPrefixWords(current.children[index], prefix, results);
+                prefix.deleteCharAt(prefix.length() - 1);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
 
@@ -65,5 +136,14 @@ public class Trie {
         trie.insert("so");
 
         System.out.println("Words inserted successfully !!");
+
+        System.out.println(trie.search("so"));
+
+
+        List<String> suggestions = trie.autocomplete("ca");
+
+        for (String suggestion : suggestions) {
+            System.out.println(suggestion);
+        }
     }
 }
